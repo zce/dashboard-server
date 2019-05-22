9972966b0d41d46bd7ba16ac5b14cd98015a22e9
zce <w@zce.me>
2019-05-22 17:02:06 +0800

    feat: google cloud storage

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./lib')
 
-server.listen(3000, () => {
-  console.log('JSON Server is running: http://localhost:3000')
+server().then(app => {
+  app.listen(3000, () => {
+    console.log('JSON Server is running: http://localhost:3000')
+  })
 })
